508ac98f5af7b109706605a4e899df16b10010d7
Bipin Karki <bipinkarki172@gmail.com>
2020-09-06 10:24:46 +0545

    Send & get requests to mongodb

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Pusher from 'pusher-js';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import axios from "./axios"
 
 function App() {
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get('/messages/sync')
+      .then(response => {
+        setMessages(response.data);
+      })
+  }, []);
 
   useEffect(() => {
     const pusher = new Pusher('b4bce440702782b3d04c', {
@@ -12,14 +22,19 @@ function App() {
     });
 
     const channel = pusher.subscribe('messages');
-    channel.bind('inserted', function(data) {
-      alert(JSON.stringify(data));
+    channel.bind('inserted', function (newMessage) {
+      // alert(JSON.stringify(newMessage));
+      setMessages([...messages, newMessage]);
     });
-   
+
     return () => {
-      
+      channel.unbind_all();
+      channel.unsubscribe();
     }
-  }, [])
+  }, [messages]);
+
+  // console.log(messages);
+
 
   return (
     <div className="app">
@@ -29,7 +44,9 @@ function App() {
         <Sidebar />
 
         {/* Chat */}
-        <Chat />
+        <Chat 
+        messages={messages}
+         />
       </div>
 
 
--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Chat.css";
 import { Avatar, IconButton } from '@material-ui/core';
 import { SearchOutlined, AttachFile, MoreVert } from '@material-ui/icons';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import MicIcon from '@material-ui/icons/Mic';
+import axios from './axios';
 
-function Chat() {
+function Chat({ messages }) {
+
+    const [input, setInput] = useState('');
+    const sendMessage = async (e) => {
+        e.preventDefault();
+
+        axios.post("/messages/new", {
+            message: input,
+            name: "Bp1",
+            timestamp: new Date().toString(),
+            recieved: false
+        });
+
+        setInput('');
+
+    }
+
+
     return (
         <div className="chat">
             <div className="chat_header">
@@ -34,58 +52,24 @@ function Chat() {
             </div>
 
             <div className="chat__body">
-                <p className="chat_message">
-                    <span className="chat__name"> Bipin</span>
-                    This is message
-                    <span className="chat__timestamp">{new Date().toString()}</span>
-                </p>
-                <p className="chat_message chat__reciever">
-                    <span className="chat__name"> Bipin</span>
-                    This is message
-                    <span className="chat__timestamp">{new Date().toString()}</span>
-                </p>
-                <p className="chat_message">
-                    <span className="chat__name"> Bipin</span>
-                    This is message
-                    <span className="chat__timestamp">{new Date().toString()}</span>
-                </p>
-                <p className="chat_message">
-                    <span className="chat__name"> Bipin</span>
-                    This is message
-                    <span className="chat__timestamp">{new Date().toString()}</span>
-                </p>
-                <p className="chat_message chat__reciever">
-                    <span className="chat__name"> Bipin</span>
-                    This is message
-                    <span className="chat__timestamp chat__reciever">{new Date().toString()}</span>
-                </p>
-                <p className="chat_message">
-                    <span className="chat__name"> Bipin</span>
-                    This is message
-                    <span className="chat__timestamp chat__reciever">{new Date().toString()}</span>
-                </p>
-                <p className="chat_message">
-                    <span className="chat__name"> Bipin</span>
-                    This is message
-                    <span className="chat__timestamp">{new Date().toString()}</span>
-                </p>
-                <p className="chat_message chat__reciever">
-                    <span className="chat__name"> Bipin</span>
-                    This is message
-                    <span className="chat__timestamp">{new Date().toString()}</span>
-                </p>
-                <p className="chat_message">
-                    <span className="chat__name"> Bipin</span>
-                    This is message
-                    <span className="chat__timestamp">{new Date().toString()}</span>
-                </p>
+                {
+                    messages.map(message => (
+                        <p className={`chat_message ${message.recieved && "chat__reciever"}`}>
+                            <span className="chat__name">{message.name}</span>
+                            {message.message}
+                            <span className="chat__timestamp">{message.timestamp}</span>
+                        </p>
+                    ))
+                }
+
+
             </div>
 
             <div className="chat__footer">
                 <InsertEmoticonIcon />
                 <form>
-                    <input placeholder="Type a message" type="text" />
-                    <button type="submit">Send a message</button>
+                    <input value={input} onChange={e => setInput(e.target.value)} placeholder="Type a message" type="text" />
+                    <button onClick={sendMessage} type="submit">Send a message</button>
                 </form>
                 <MicIcon />
             </div>

7d791e5e45acdba8ad029b3a0867bd8eac2edeae
Bipin Karki <bipinkarki172@gmail.com>
2020-09-06 09:34:30 +0545

    listen to pusher from backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Pusher from 'pusher-js';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 
 function App() {
+
+  useEffect(() => {
+    const pusher = new Pusher('b4bce440702782b3d04c', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      alert(JSON.stringify(data));
+    });
+   
+    return () => {
+      
+    }
+  }, [])
+
   return (
     <div className="app">
 
